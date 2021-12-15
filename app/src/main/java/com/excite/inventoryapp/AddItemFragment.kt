8b6ae09d7edc8b10960d75f0b223a3942ca5a4b1
excitexcite <excitenew@gmail.com>
2021-12-15 15:25:08 +0200

    Implement ability to add item to database

--- app/src/main/java/com/excite/inventoryapp/AddItemFragment.kt
+++ app/src/main/java/com/excite/inventoryapp/AddItemFragment.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.excite.inventoryapp.data.Item
 import com.excite.inventoryapp.databinding.FragmentAddItemBinding
 
 /**
@@ -16,6 +19,16 @@ import com.excite.inventoryapp.databinding.FragmentAddItemBinding
 class AddItemFragment : Fragment() {
 
     private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    // to share the ViewModel across fragments.
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
+    lateinit var item: Item
 
     // Binding object instance corresponding to the fragment_add_item.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
@@ -30,6 +43,45 @@ class AddItemFragment : Fragment() {
     ): View? {
         _binding = FragmentAddItemBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    /**
+     * Called when the view is created.
+     * The itemId Navigation argument determines the edit item  or add new item.
+     * If the itemId is positive, this method retrieves the information from the database and
+     * allows the user to update it.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveAction.setOnClickListener {
+            addNewItem()
+        }
+    }
+
+    /**
+     * Inserts the new Item into database and navigates up to list fragment.
+     */
+    private fun addNewItem() {
+        if (isEntryValid()) {
+            viewModel.addNewItem(
+                binding.itemName.text.toString(),
+                binding.itemPrice.text.toString(),
+                binding.itemCount.text.toString(),
+            )
+            val action = AddItemFragmentDirections.actionAddItemFragmentToItemListFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+    /**
+     * Returns true if the EditTexts are not empty
+     */
+    private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            binding.itemName.text.toString(),
+            binding.itemPrice.text.toString(),
+            binding.itemCount.text.toString(),
+        )
     }
 
     /**
--- app/src/main/java/com/excite/inventoryapp/InventoryApplication.kt
+++ app/src/main/java/com/excite/inventoryapp/InventoryApplication.kt
@@ -1,5 +1,10 @@
 package com.excite.inventoryapp
 
 import android.app.Application
+import com.excite.inventoryapp.data.ItemRoomDatabase
 
-class InventoryApplication : Application()
+class InventoryApplication : Application() {
+    // Using by lazy so the database is only created when needed
+    // rather than when the application starts
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+}
